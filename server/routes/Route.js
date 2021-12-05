b3728d05243e335b5e19290c6fd5a8817a41fd40
sumanthhegde55 <74821622+sumanthhegde55@users.noreply.github.com>
2021-12-06 02:54:40 +0530

    Delete server directory

--- server/routes/Route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {addUser , getUsers} from '../controller/UserController.js';
-
-const route = express.Router();
-
-route.post('/add',addUser);
-route.get('/users',getUsers);
-
-export default route;
